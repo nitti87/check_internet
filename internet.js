dbc4f1aafc36d83a7b204822e76af7d53f5bb0f4
nitti87 <44554259+nitti87@users.noreply.github.com>
2020-06-09 12:05:43 +0200

    Update internet.js

--- internet.js
+++ internet.js
@@ -1,10 +1,12 @@
 const check_internet = (option = {}) => {
-  let [ with_interval, timer, startOver, interval, doAfter_offline ] = [
+  let [ with_interval, timer, startOver, interval, doAfter_offline, number, count ] = [
     option.with_interval || false, 
     undefined, 
     false,
     option.with_interval ? option.with_interval.interval || 1000 : undefined,
-    option.offline.doAfter_offline || null
+    option.offline.doAfter_offline || null,
+    option.offline.count && typeof option.offline.count.do === 'function' ? parseInt(option.offline.count.number) : undefined,
+    option.offline.count && typeof option.offline.count.do === 'function' ? 0 : undefined
   ]
 
   const start_interval = () => {
@@ -18,11 +20,32 @@ const check_internet = (option = {}) => {
       typeof option.online.run === 'function' ? option.online.run(check_funct) : option.online.run || null
       with_interval && startOver && !doAfter_bool ? (start_interval(), startOver = false) : null
     } else {
-      typeof option.offline.run === 'function' ? option.offline.run(check_funct) : option.offline.run || null
-      clearInterval(timer)
-      startOver = true
+      
+      if(count === number) {
+        clearInterval(timer)
+        return counted_error()
+      } else {        
+        
+        typeof option.offline.run === 'function' ? option.offline.run(check_funct) : option.offline.run || null
+        clearInterval(timer)
+        startOver = true
+        
+        option.offline.count && typeof option.offline.count.do === 'function' ? count++ : null
+      }
     }
   }
   
+  function counted_error(){
+    option.offline.count.do()
+    
+    let time_error = setInterval(() => {
+      if(navigator.onLine) {
+        doAfter_offline()
+        clearInterval(time_error)
+        start_interval()
+      }
+    }, interval || 1000)
+  }
+
   start_interval()
 }
